2f9667dbd2776087c3b56ff9074acd652b92f7b1
lujunfa <2990606482@qq.com>
2018-04-06 17:53:54 +0800

    springboot学习demon代码

---
+++ springboot-learning-example/chapter-5-spring-boot-paging-sorting/src/main/java/spring/boot/core/domain/UserRepository.java
@@ -0,0 +1,13 @@
+package spring.boot.core.domain;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.RepositoryDefinition;
+
+/**
+ * 用户持久层操作接口
+ *
+ * Created by bysocket on 18/09/2017.
+ */
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+}
